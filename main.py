cc57f276cab4be52f82d340f7dc649c3f0fce52d
bechirMakhlouf <bechirM@proton.me>
2023-12-27 16:14:42 +0100

    add modele alpha

--- main.py
+++ main.py
@@ -33,18 +33,6 @@ modele_theta_questions = [
         "question": " Comment réagis-tu face aux situations stressantes ou difficiles?",
         "reponses": ["oui", "non"],
     },
-    {
-        "question": "As-tu ressenti de la fatigue ou de la somnolence récemment",
-        "reponses": ["oui", "non"],
-    },
-    {
-        "question": "As-tu l'impression d'avoir suffisamment dormi ?",
-        "reponses": ["oui", "non"],
-    },
-    {
-        "question": "Suis-tu une routine de sommeil régulière ?",
-        "reponses": ["oui", "non"],
-    },
 ]
 
 modele_gamma_questions = [
@@ -60,7 +48,32 @@ modele_gamma_questions = [
         "questions": "Penses-tu que l'engagement dans une tâche spécifique influence ton niveau d'éveil?",
         "reponses": ["oui", "non"],
     },
+
+    {
+        "question": "As-tu ressenti de la fatigue ou de la somnolence récemment",
+        "reponses": ["oui", "non"],
+    },
+    {
+        "question": "As-tu l'impression d'avoir suffisamment dormi ?",
+        "reponses": ["oui", "non"],
+    },
+    {
+        "question": "Suis-tu une routine de sommeil régulière ?",
+        "reponses": ["oui", "non"],
+    },
+
 ]
+
+modele_alpha_questions = [ 
+ { "question": "As-tu ressenti une diminution de stress récemment ?", },
+ { "question": "Trouves-tu certaines activités particulièrement relaxantes ?", },
+ { "question": "Pratiques-tu des techniques de relaxation, comme la méditation ou la respiration profonde ?", },
+ { "question": "Penses-tu que ton environnement actuel contribue à la relaxation ?", },
+ { "question": "Penses-tu que la qualité de ton sommeil influence ta détente ?", },
+ { "question": "Peux-tu identifier des signes physiques qui indiquent que tu es détendu ?"  },
+ { "question": "Prends-tu le temps de te détendre dans ta journée ?" }
+ ]
+
 themes = [
     {
         "theme": "une phase de sommeil plus profonde",
