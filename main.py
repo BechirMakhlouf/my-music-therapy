4d482633fa4d812e6a95608c80d434ea454da16c
bechirMakhlouf <bechirM@proton.me>
2023-12-27 15:49:10 +0100

    ajouter questions

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
+#!/usr/bin/python3
+print("")
 
-print("Hello world");
+modele_delta_questions = [
+    {
+        "question": "As-tu récemment eu des rêves qui t'ont semblé effrayants ou dérangeants ? oui/nonAs-tu récemment eu des rêves qui t'ont semblé effrayants ou dérangeants ?",
+        "reponses": ["oui", "non"],
+    },
+    {
+        "question": "est-ce que tu as des cauchemars qui ont un impact sur ta vie quotidienne?",
+        "reponses": ["oui", "non"],
+    },
+]
+
+modele_theta_questions = [
+    {
+        "question": " Y a-t-il des émotions spécifiques que tu ressens fréquemment ces derniers temps ? ",
+        "reponses": ["oui", "non"],
+    },
+    {
+        "question": " Comment réagis-tu face aux situations stressantes ou difficiles?",
+        "reponses": ["oui", "non"],
+    },
+]
+
+themes = [
+    {
+        "theme": "une phase de sommeil plus profonde",
+        "questions" =  modele_delta_questions,
+    },
+    {
+        "theme": "améliorer la méditation, la créativité",
+            "questions": 
+    },
+    {
+        "theme": "La relaxation"
+    },
+
+]
+
+reponse1 = input()
